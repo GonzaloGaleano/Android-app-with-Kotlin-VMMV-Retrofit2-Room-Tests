c2c0c09f15b4bf10db5256245b8fa9813c703d70
Gonzalo Galeano <gozpellz@gmail.com>
2022-06-25 13:31:17 -0400

    config/prepare viewBinding + dataBinding

--- app/src/main/java/com/example/jsonplaceholderposts/ui/PostsActivity.kt
+++ app/src/main/java/com/example/jsonplaceholderposts/ui/PostsActivity.kt
@@ -2,15 +2,18 @@ package com.example.jsonplaceholderposts.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import androidx.activity.viewModels
-import com.example.jsonplaceholderposts.R
+import com.example.jsonplaceholderposts.databinding.ActivityPostsBinding
 
 
 class PostsActivity : AppCompatActivity() {
     private val viewModel: PostListViewModel by viewModels()
+    private lateinit var binding: ActivityPostsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_posts)
+        binding = ActivityPostsBinding.inflate(LayoutInflater.from(this))
+        setContentView(binding.root)
         observerPost()
     }
 
