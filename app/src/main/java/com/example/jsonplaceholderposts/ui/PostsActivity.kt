384e22f2d62a69896220f2df9b3fc39c3435951e
Gonzalo Galeano <gozpellz@gmail.com>
2022-06-25 11:53:13 -0400

    PostsActivity base

--- app/src/main/java/com/example/jsonplaceholderposts/ui/PostsActivity.kt
+++ app/src/main/java/com/example/jsonplaceholderposts/ui/PostsActivity.kt
@@ -1,11 +1,12 @@
-package com.example.jsonplaceholderposts
+package com.example.jsonplaceholderposts.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.jsonplaceholderposts.R
 
-class MainActivity : AppCompatActivity() {
+class PostsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_posts)
     }
 }
